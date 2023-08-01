919203b4960683f0a5df1a4253acb65d19f553b2
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2023-08-01 10:57:23 +0200

    add multiline comment

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 /* This is the firstname of the user */
 
 const user = 'John'
-
+  
 
 /* This is the lastname of the user */
  const surname = 'Smith'
@@ -9,7 +9,7 @@ const user = 'John'
 
 console.log(user, surname);
 
-
+//challenge 2:
 /**  (c) ACME Inc. 2010 */
 
 /** This is the date that a user created their account 
@@ -19,6 +19,19 @@ const date = '10/07/2014'
 
 console.log(date)
 
+
+//Challenge 3 //
+/*It is important to show the following message in order to:
+- to assure users
+- to scare hackers
+- to impress investors
+*/
+
+console.warn(Security scan starting);
+
+/* It is important to let user know when they can close the page  /*
+
+cnosole.info('Please wait for scan to complete before closing the browser.)
 
 
  
